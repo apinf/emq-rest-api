93f696ac440f270fcbceedb1cc36fdc7ba0aeefb
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-01-26 16:20:48 +0200

    Add emq-acl model and controller

---
+++ api/controllers/Emq-aclController.js
@@ -0,0 +1,11 @@
+/**
+ * Emq-aclController
+ *
+ * @description :: Server-side logic for managing emq-acls
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
